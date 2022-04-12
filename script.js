4031561acf084789ebbad4022d7bd406d944a932
rendrarya <rendrarya12345@gmail.com>
2022-04-12 18:31:31 +0800

    menambahkan function random ganti warna kotak

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ document.onmousemove = (event) => {
     kotak1.style.left = x - (kotak1.clientWidth/2) + "px";
     kotak1.style.top = y - (kotak1.clientWidth/2) + "px";
     }
+    function random(angka){
+        return Math.floor(Math.random() * angka)
+    }
+    kotak1.onclick = function () {
+    ganti = "rgb("+random(255)+", "+random(255)+", "+random(255)+")";
+    kotak1.style.backgroundColor = ganti;
+    }
