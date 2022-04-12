f28ac64eab658f8bff10a1f0edd6bc5322acdfaa
rendrarya <rendrarya12345@gmail.com>
2022-04-12 18:21:05 +0800

    mouse di klik ganti warna

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ document.onmousemove = (event) => {
     var y = event.clientY;
     kotak1.style.left = x - (kotak1.clientWidth/2) + "px";
     kotak1.style.top = y - (kotak1.clientWidth/2) + "px";
-}
+    }
